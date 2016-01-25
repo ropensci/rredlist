7950ecbf7840b0777ae6f6ca6d42d9f383e807cd
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-25 12:23:53 -0800

    updated description file, pkg level man file

--- R/rredlist-package.R
+++ R/rredlist-package.R
@@ -1,9 +1,22 @@
-#' rredlist
+#' rredlist - IUCN Red List Client
+#'
+#' @section Authentication:
+#' IUCN requires you to get your own API key, an alphanumeric string that you
+#' need to send in every request. Get it at \url{http://apiv3.iucnredlist.org/api/v3/token}.
+#' Keep this key private. You can pass the key in to each function via the
+#' \code{key} parameter, but it's better to store the key either as a environment
+#' variable (\code{IUCN_REDLIST_KEY}) or an R option (\code{iucn_redlist_key}) - we
+#' suggest using the former option.
+#'
+#' @section No Spatial:
+#' This package does not include support for the spatial API, described at
+#' \url{http://apiv3.iucnredlist.org/spatial}
 #'
 #' @importFrom httr GET content stop_for_status
 #' @importFrom jsonlite fromJSON
 #' @name rredlist-package
 #' @aliases rredlist
 #' @docType package
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @keywords package
 NULL
