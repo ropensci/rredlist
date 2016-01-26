a468b94ff322ca663deecedfb947d905fc985537
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-26 12:04:34 -0800

    remove errant code

--- R/rl_version.R
+++ R/rl_version.R
@@ -10,8 +10,3 @@
 rl_version <- function(key = NULL, ...) {
   rl_parse(rr_GET("version", key, ...), TRUE)$version
 }
-
-rl_citation <- function(key = NULL, ...) {
-  sprintf('IUCN 2015. IUCN Red List of Threatened Species. Version %s <www.iucnredlist.org>',
-          rl_version(key, ...))
-}
