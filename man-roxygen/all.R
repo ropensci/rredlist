869de15aba602300e53d2a8c534ffaf20c587acb
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-07-16 12:30:09 +0200

    add function to get key, its tests and docs. new roxygen2 version

--- man-roxygen/all.R
+++ man-roxygen/all.R
@@ -1,5 +1,4 @@
-#' @param key A IUCN API token. See
-#' \url{http://apiv3.iucnredlist.org/api/v3/token} to get a token
+#' @param key A IUCN API token. See \code{\link{rl_use_iucn}}.
 #' @param ... Curl options passed to \code{\link[crul]{HttpClient}}
 #' @param parse (logical) Whether to parse to list (\code{FALSE}) or
 #' data.frame (\code{TRUE}). Default: \code{TRUE}
