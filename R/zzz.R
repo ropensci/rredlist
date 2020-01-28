941b5e603e936d7614f2c19e0660c3f0fca3d971
Scott Chamberlain <myrmecocystus@gmail.com>
2020-01-27 17:26:42 -0800

    no more R oldrel on travis - fix if test in assert_is method

--- R/zzz.R
+++ R/zzz.R
@@ -49,7 +49,7 @@ space <- function(x) gsub("\\s", "%20", x)
 
 assert_is <- function(x, y) {
   if (!is.null(x)) {
-    if (!class(x) %in% y) {
+    if (!inherits(x, y)) {
       stop(deparse(substitute(x)), " must be of class ",
            paste0(y, collapse = ", "), call. = FALSE)
     }
