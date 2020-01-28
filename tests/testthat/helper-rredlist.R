fb698e6e3993d2a7e272aa8a471ca0fe6afdc2f7
Scott Chamberlain <myrmecocystus@gmail.com>
2020-01-27 16:45:52 -0800

    re-record vcr cassettes

--- tests/testthat/helper-rredlist.R
+++ tests/testthat/helper-rredlist.R
@@ -1,6 +1,6 @@
 # set up vcr
 library("vcr")
 invisible(vcr::vcr_configure(
-    dir = "../fixtures/vcr_cassettes",
+    dir = "../fixtures",
     filter_sensitive_data = list("<<rredlist_api_token>>" = Sys.getenv('IUCN_REDLIST_KEY'))
 ))
