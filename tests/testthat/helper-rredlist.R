9f821c7dd061de30132c011578058070757a284a
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-02-06 09:50:13 +0100

    like this?

--- tests/testthat/helper-rredlist.R
+++ tests/testthat/helper-rredlist.R
@@ -1,7 +1,15 @@
-Sys.setenv("IUCN_REDLIST_KEY" = "foobar")
 # set up vcr
 library("vcr")
 invisible(vcr::vcr_configure(
     dir = "../fixtures",
     filter_sensitive_data = list("<<rredlist_api_token>>" = Sys.getenv('IUCN_REDLIST_KEY'))
 ))
+
+if (!nzchar(Sys.getenv("IUCN_REDLIST_KEY"))) {
+  if (dir.exists(vcr::vcr_configuration()$dir)) {
+    Sys.setenv("IUCN_REDLIST_KEY" = "foobar")
+  } else {
+    stop("No API key nor cassettes, tests cannot be run.")
+  }
+}
+
