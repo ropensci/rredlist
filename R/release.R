1b05b0d4bc5ee81a17d949820c5d04341f1cc520
William Gearty <willgearty@gmail.com>
2024-09-30 14:23:55 -0400

    Add release questions; update codemeta.json

---
+++ R/release.R
@@ -0,0 +1,6 @@
+release_questions <- function() {
+  c(
+    "Did you rebuild the codemeta.json file using codemetar?",
+    "Did you re-build the vignette(s) using `vignettes/precompile.R`?"
+  )
+}
