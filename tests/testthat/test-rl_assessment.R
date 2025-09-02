b6e2c8634de5da0d4e48f6149db50604997f79da
William Gearty <willgearty@gmail.com>
2025-09-02 15:39:08 -0400

    Allow users to specify multiple elements to extract (#80)
    * Allow users to specify multiple elements to extract
    
    * Add tests for vectorized extraction
    
    * Use vectorized rl_assessment_extract() in vignette; recompile vignettes

--- tests/testthat/test-rl_assessment.R
+++ tests/testthat/test-rl_assessment.R
@@ -135,12 +135,31 @@ test_that("rl_assessment_extract works", {
   expect_equal(colnames(extract7)[1], "assessment_id")
   expect_equal(extract7$kingdom_name, extract6$kingdom_name)
 
+  # test with a vector of elements to extract
+  extract8 <- rl_assessment_extract(aa, c("taxon", "threats"))
+  expect_is(extract8, "list")
+  expect_length(extract8, 2)
+  expect_is(extract8[[1]], "list")
+  expect_is(extract8[[2]], "list")
+  expect_true("kingdom_name" %in% names(extract8[[1]]))
+  expect_true("code" %in% names(extract8[[1]]))
+
+  extract9 <- rl_assessment_extract(aa, c("taxon", "threats"),
+                                    format = "df", flatten = TRUE)
+  expect_is(extract9, "data.frame")
+  expect_equal(colnames(extract9)[1], "assessment_id")
+  expect_true("kingdom_name" %in% colnames(extract9))
+  expect_true("code" %in% colnames(extract9))
+
   expect_error(rl_assessment_extract(), "is missing, with no default")
   expect_error(rl_assessment_extract(lst = 166290968, el_name = "taxon"),
                "lst must be of class list")
   expect_error(rl_assessment_extract(aa, el_name = 5),
                "el_name must be of class character")
-  expect_error(rl_assessment_extract(aa, el_name = "abcdefg"))
+  expect_error(rl_assessment_extract(aa, el_name = "abcdefg"),
+               "not found")
+  expect_error(rl_assessment_extract(aa, c("taxon", "abcdefg")),
+               "not found")
   expect_error(rl_assessment_extract(aa, el_name = "taxon", format = TRUE),
                "format must be of class character")
   expect_error(rl_assessment_extract(aa, el_name = "taxon", format = "test"))
